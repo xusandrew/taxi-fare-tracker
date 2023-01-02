8f187a675891a2c219367c346007fbc3e28d22a4
Andrew Xu <xusandrew@gmail.com>
2023-01-02 04:47:21 -0500

    Fix prices not being stored

--- City.py
+++ City.py
@@ -1,6 +1,6 @@
 class City:
     def __init__(self, name, taxi_start, taxi_perkm, currency):
         self.name = name
-        self.taxi_start = float(taxi_start)
-        self.taxi_perkm = float(taxi_perkm)
+        self.taxi_start = float(taxi_start.replace(',', ''))
+        self.taxi_perkm = float(taxi_perkm.replace(',', ''))
         self.currency = currency
