6c2b799ec71d9451b20716f74fe1e16120c779d4
Andrew Xu <xusandrew@gmail.com>
2023-01-01 23:17:05 -0500

    Make list of class for each country

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from City import City
+from Country import Country
 
 # get HTML from page
 url = "https://www.numbeo.com/taxi-fare/"
@@ -9,3 +11,10 @@ text = requests.get(url=url).text
 soup = BeautifulSoup(text, 'html.parser')
 links = soup.find_all('a')
 links = list(filter(lambda link: link.get('href')[0] == 'c', links))
+
+# iterate through links and generate a list of countries
+countries = []
+for link in links:
+    name = link.getText()
+    country = Country(name)
+    countries.append(country)
