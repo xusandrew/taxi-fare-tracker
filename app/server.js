cf931709e6c4beb865ba25aa83456e377cf279b4
Andrew Xu <xusandrew@gmail.com>
2023-01-07 01:58:16 -0500

    Implement get requests for city info

--- app/server.js
+++ app/server.js
@@ -6,6 +6,18 @@ const pool = require('./db')
 app.use(cors())
 app.use(express.json())
 
+app.get('/:city', async (req, res) => {
+    try {
+        const { city } = req.params
+        const data = await pool.query(
+            `SELECT * FROM taxifares WHERE city = '${city}';`
+        )
+        res.json(data.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 app.listen(5000, () => {
     console.log('Server has started on port 5000')
 })
