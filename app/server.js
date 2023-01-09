73f5fe262b41b4e07f6759ac95d25423d50f0c65
Andrew Xu <xusandrew@gmail.com>
2023-01-08 23:06:12 -0500

    Add get method for country

--- app/server.js
+++ app/server.js
@@ -6,18 +6,30 @@ const pool = require('./db')
 app.use(cors())
 app.use(express.json())
 
-app.get('/:city', async (req, res) => {
-    try {
-        const { city } = req.params
-        const data = await pool.query(
-            `SELECT * FROM taxifares WHERE city = '${city}';`
-        )
-        res.json(data.rows[0])
-    } catch (err) {
-        console.error(err.message)
-    }
+app.get('/countries/:text', async (req, res) => {
+  try {
+    const { text } = req.params
+    const data = await pool.query(
+      `SELECT * FROM taxifares WHERE country LIKE '${text}%';`
+    )
+    res.json(data.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+app.get('/city/:city', async (req, res) => {
+  try {
+    const { city } = req.params
+    const data = await pool.query(
+      `SELECT * FROM taxifares WHERE city = '${city}';`
+    )
+    res.json(data.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
 })
 
 app.listen(5000, () => {
-    console.log('Server has started on port 5000')
+  console.log('Server has started on port 5000')
 })
