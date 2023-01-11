df67df2c92cda522c581fb549584e7866daf3a92
Andrew Xu <xusandrew@gmail.com>
2023-01-11 18:14:00 -0500

    Get Chart.js working

---
+++ web/src/components/Graph.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const Graph = ({ chartData }) => {
+  const options = {}
+
+  return (
+    <>
+      <Line data={chartData} options={options}></Line>
+    </>
+  )
+}
+
+export default Graph
