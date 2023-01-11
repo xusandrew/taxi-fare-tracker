f7be09dc8329eb2953c28239d5b2402d22ac7ba5
Andrew Xu <xusandrew@gmail.com>
2023-01-10 19:33:40 -0500

    Implement upload

--- app/scripts/uber.py
+++ app/scripts/uber.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+import psycopg2
+
 from data import inputs
 
 # start up selenium
@@ -48,14 +50,50 @@ def find_prices():
     the city center, and from the city center back to the airport
     '''
 
+    output = {}
     for city_name in inputs:
         values = inputs[city_name]
         airport_to_center = scrape_links(values['airport'], values['center'])
         center_to_airport = scrape_links(values['center'], values['airport'])
-        print(city_name)
-        print("ap to c: " + str(airport_to_center))
-        print(center_to_airport)
+
+        output[city_name] = {
+            'airport_to_center': airport_to_center,
+            'center_to_airport': center_to_airport
+        }
+    return output
 
 
-find_prices()
+def get_db_queries(data):
+    pass
+
+
+def upload(data):
+    '''Upload data generated from find_prices() to PSQL database'''
+
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print("Connecting to server")
+        conn = psycopg2.connect(
+            database="taxifaretracker", user="andrew", password="")
+        print("Connected")
+
+        cur = conn.cursor()
+
+        # commands
+        for query in get_db_queries(data):
+            cur.execute(query)
+
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('DB Conn closed')
+
+
+airport_center_data = find_prices()
+upload()
 driver.quit()
