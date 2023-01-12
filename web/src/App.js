bd8fe55aca85197c47c3730f030f56559c9f6ca4
Andrew Xu <xusandrew@gmail.com>
2023-01-11 23:29:21 -0500

    Reorganize files

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import './App.css'
 
 // Components
 import Input from './components/Input'
-
-import Title from './components/Title'
 import Info from './components/Info'
 
 function App() {
   return (
-    <div id='body'>
-      <Title />
+    <div id='container'>
       <Input />
       <Info />
     </div>
--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+
 import App from './App'
+import Title from './components/Title'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Title />
+    <App />
+  </React.StrictMode>
 )
