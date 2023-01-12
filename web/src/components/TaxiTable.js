94671fea3df9349bc2e371a15b2b425eb98e241e
Andrew Xu <xusandrew@gmail.com>
2023-01-12 14:26:56 -0500

    Basic Table layout

---
+++ web/src/components/TaxiTable.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const TaxiTable = props => {
+  return (
+    <>
+      <div class='table-wrapper'>
+        <table>
+          <thead>
+            <tr>
+              <th>Distance</th>
+              <th>Price</th>
+              <th>Distance</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <tr>
+              <td>Item One</td>
+              <td>Ante turpis integer aliquet porttitor.</td>
+              <td>29.99</td>
+            </tr> */}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default TaxiTable
