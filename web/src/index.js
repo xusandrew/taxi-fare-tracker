7aa8f34b3dd39ba40a5f8c2dc1f79c5a9cb19cd6
Andrew Xu <xusandrew@gmail.com>
2023-01-15 15:49:17 -0500

    Edit HTML

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 import App from './App'
-
-if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
