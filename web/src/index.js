0eab9b463dd348b2169cc4b9872ecf7787a6282d
Andrew Xu <xusandrew@gmail.com>
2023-01-15 14:23:05 -0500

    Fix selenium timeout issue

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 import App from './App'
+
+if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
